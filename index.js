bc869224aceee77899c383b215af693c471b4ad9
aniketkolhe43 <aniketkolhe0907@gmail.com>
2022-09-28 23:34:29 +0530

    day2

---
+++ index.js
@@ -0,0 +1,10 @@
+arr1 = ["About Us", "UC Impact", "Terms & Condition", "Privacy Policy",
+ "Intrest-Based Advertising","Anti Discrimination Policy","Blog","Review",
+ "Near Me","Careers","Gift Cards","Contact US","Quick Links"]
+
+ usa=["Austin","Dallas","New York City"]
+
+ ind= ["Agar","Ahemedabad","Alwar","Amritsar","Aurangabad","Banglore",
+"Bhopal","Bhubaneswar","Chandigarh tricity","Chenaai","Coimbatore","Cuttack",
+""]
+
